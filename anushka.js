50752edd1b9dd0e8b84930684b500c178df28f6e
shuklanushka <anushkavshukla28@gmail.com>
2022-12-25 01:38:46 +0530

    first commit

---
+++ anushka.js
@@ -0,0 +1,11 @@
+/*
+hello
+*/
+const myBirthYear = 2004
+const myAge = 2022- myBirthYear
+if(myAge>=18){
+    console.log(`Age is ${myAge}, which is above 18.`)
+}
+else{
+    console.log(`Age is less than 18`)
+}
